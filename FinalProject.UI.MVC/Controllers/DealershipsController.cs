0c683d24b54f442ccfb4a9a12ebcab065967f46b
dplackard <dplackard@outlook.com>
2020-04-13 16:02:52 -0500

    Day 1 Commit - Template conversion, added identity, and database implementation.

---
+++ FinalProject.UI.MVC/Controllers/DealershipsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalProject.DATA.EF;
+
+namespace FinalProject.UI.MVC.Controllers
+{
+    public class DealershipsController : Controller
+    {
+        private FinalProjectEntities db = new FinalProjectEntities();
+
+        // GET: Dealerships
+        public ActionResult Index()
+        {
+            var dealerships = db.Dealerships.Include(d => d.Car);
+            return View(dealerships.ToList());
+        }
+
+        // GET: Dealerships/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dealership dealership = db.Dealerships.Find(id);
+            if (dealership == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dealership);
+        }
+
+        // GET: Dealerships/Create
+        public ActionResult Create()
+        {
+            ViewBag.CarId = new SelectList(db.Cars, "CarId", "Make");
+            return View();
+        }
+
+        // POST: Dealerships/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DealershipId,DealershipName,Address,City,State,ZipCode,CarId,PhoneNumber")] Dealership dealership)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Dealerships.Add(dealership);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CarId = new SelectList(db.Cars, "CarId", "Make", dealership.CarId);
+            return View(dealership);
+        }
+
+        // GET: Dealerships/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dealership dealership = db.Dealerships.Find(id);
+            if (dealership == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CarId = new SelectList(db.Cars, "CarId", "Make", dealership.CarId);
+            return View(dealership);
+        }
+
+        // POST: Dealerships/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DealershipId,DealershipName,Address,City,State,ZipCode,CarId,PhoneNumber")] Dealership dealership)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dealership).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CarId = new SelectList(db.Cars, "CarId", "Make", dealership.CarId);
+            return View(dealership);
+        }
+
+        // GET: Dealerships/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dealership dealership = db.Dealerships.Find(id);
+            if (dealership == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dealership);
+        }
+
+        // POST: Dealerships/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Dealership dealership = db.Dealerships.Find(id);
+            db.Dealerships.Remove(dealership);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
